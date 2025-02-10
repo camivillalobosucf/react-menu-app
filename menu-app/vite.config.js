269de6db8ba9607c06ff18c92d3ca5e8e3bd4dd8
Camila Villalobos <ca575679@ucf.edu>
2025-02-10 17:21:27 -0500

    Delete menu-app directory

--- menu-app/vite.config.js
+++
@@ -1,8 +0,0 @@
-//this is a setup so vite can work semamsly with react
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [react()],
-});
